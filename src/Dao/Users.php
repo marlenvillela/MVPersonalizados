5897f156a988e4512900be76333e225d3c4f4ade
DSoto7 <sotoalvarez1500@gmail.com>
2025-12-11 12:46:08 -0600

    Update Users.php
    Actualizacion en src/Dao/Users.php

--- src/Dao/Users.php
+++ src/Dao/Users.php
@@ -5,17 +5,21 @@ class Users extends Table
 {
     public function getByUsername($username)
     {
+        // Preparar consulta para buscar un usuario especifico
         $stmt = $this->pdo->prepare("SELECT * FROM users WHERE userName = :u LIMIT 1");
+        // Ejecutar consulta con el nombre de usuario
         $stmt->execute([":u" => $username]);
         return $stmt->fetch(\PDO::FETCH_ASSOC);
     }
 
     public function create($username, $hashedPassword)
     {
+         // Consulta para insertar un nuevo usuario
         $stmt = $this->pdo->prepare("
             INSERT INTO users (userName, userPassword, userRole)
             VALUES (:u, :p, 'CLIENT')
         ");
+         // Ejecuta la consulta con los parametros enviados
         return $stmt->execute([
             ":u" => $username,
             ":p" => $hashedPassword
