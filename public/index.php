43746d7e295cc009825c6d8cf033bbc520d2b0a0
graymonsta <jusanchez708@unicah.edu>
2025-12-11 20:24:20 -0600

    cambioss

--- public/index.php
+++ public/index.php
@@ -1,16 +1,16 @@
 <?php
-// 1. Autoload de Composer (NECESARIO para Dotenv)
+// 1. Autoload de Composer (NECESARIO para Dotenv).
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // 2. Cargar el archivo .env
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->safeLoad(); // usa safeLoad() para evitar errores si falta algo
+$dotenv->safeLoad(); // usa safeLoad() para evitar errores si falta algo.
 
-// 3. Autoload interno del framework
+// 3. Autoload interno del framework.
 require_once __DIR__ . '/../src/Core/Autoload.php';
 \App\Core\Autoload::register();
 
-// 4. Iniciar router
+// 4. Iniciar router.
 use App\Core\Router;
 
 $router = new Router();
